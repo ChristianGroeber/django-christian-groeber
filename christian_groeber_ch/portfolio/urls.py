d467de3f82494f6a21d1ce7f4a608c51d63e906b
ChristianGroeber <christian.groeber@bluewin.ch>
2019-04-10 00:16:48 +0200

    added portfolio page

--- christian_groeber_ch/portfolio/urls.py
+++ christian_groeber_ch/portfolio/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls import url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.portfolio, name='portfolio'),
+    url('<portfolio_type>/', views.spec_portfolio, name='spec_portfolio')
 ]
--- christian_groeber_ch/portfolio/views.py
+++ christian_groeber_ch/portfolio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Type
@@ -9,3 +9,8 @@ def portfolio(request):
     for type in types:
         type.generate_url()
     return render(request, 'portfolio/index.html', {'types': types})
+
+
+def spec_portfolio(request, portfolio_type):
+    print(portfolio_type)
+    return redirect('index')
