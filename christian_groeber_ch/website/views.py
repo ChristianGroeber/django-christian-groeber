e75191f50e0d09989bc387712e6316307d69a2f3
ChristianGroeber <christian.groeber@bluewin.ch>
2019-04-12 17:24:36 +0200

    added csr file

--- christian_groeber_ch/website/views.py
+++ christian_groeber_ch/website/views.py
@@ -1,7 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from christian_groeber_ch.settings import STATICFILES_DIRS
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'website/index.html')
+
+
+def csr(request):
+    print(STATICFILES_DIRS)
+    f = open(STATICFILES_DIRS[0] + '/csr/C3D56FF12F47C32F673F55F046ADFE3C.txt', 'r')
+    file_content = f.read()
+    f.close()
+    return HttpResponse(file_content, content_type="text/plain")
