f732f8c671cd65a3db90a6f8fc13af4d8e374a45
ChristianGroeber <christian.groeber@bluewin.ch>
2019-04-30 11:52:22 +0200

    portfolio elements and experiences get sorted

--- christian_groeber_ch/hire_me/views.py
+++ christian_groeber_ch/hire_me/views.py
@@ -8,5 +8,21 @@ from .models import Resume, Experience
 
 def index(request):
     resume = Resume.objects.get(pk=1)
-    experiences = resume.experiences.all().order_by('-date_from')
-    return render(request, 'hire_me/index.html', {'resume': resume, 'experiences': experiences})
+    exp = resume.experiences.all().order_by('-date_from')
+    elements = Element.objects.all().order_by('-date_started')
+    experiences = []
+    for experience in exp:
+        experiences.append(ExperienceObject(experience.title, experience.date_from, description=experience.description, date_until=experience.date_until))
+    for element in elements:
+        experiences.append(ExperienceObject(element.title, element.date_started, description=element.description, date_until=element.date_finished))
+    print(experiences)
+    sorted_experiences = sorted(experiences, key=lambda test: test.date_from)
+    return render(request, 'hire_me/index.html', {'resume': resume, 'experiences': sorted_experiences})
+
+
+class ExperienceObject:
+    def __init__(self, title, date_from, description=None, date_until=None):
+        self.title = title
+        self.date_from = date_from
+        self.description = description
+        self.date_until = date_until
