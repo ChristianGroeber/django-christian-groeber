dd388f50d088ed9965e2fb11f4584dbbf7baa99c
ChristianGroeber <christian.groeber@bluewin.ch>
2019-04-30 10:03:42 +0200

    small changes

--- christian_groeber_ch/hire_me/models.py
+++ christian_groeber_ch/hire_me/models.py
@@ -9,7 +9,7 @@ class Experience(Model):
     title = CharField(max_length=50)
     description = CharField(max_length=500, blank=True)
     date_from = models.DateField()
-    date_until = models.DateField(auto_now=True, blank=True)
+    date_until = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return str(self.date_from) + " - " + str(self.title)
