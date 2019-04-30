49fb47f9710209347245cf335fb3e33e0ca3e3e0
ChristianGroeber <christian.groeber@bluewin.ch>
2019-04-30 10:09:23 +0200

    first view

--- christian_groeber_ch/hire_me/views.py
+++ christian_groeber_ch/hire_me/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
+from portfolio.models import Element
+from .models import Resume, Experience
 
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'hire_me/index.html')
+    resume = Resume.objects.get(pk=1)
+    experiences = resume.experiences.all().order_by('-date_from')
+    print(resume)
+    return render(request, 'hire_me/index.html', {'resume': resume, 'experiences': experiences})
