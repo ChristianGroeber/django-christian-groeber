075957b9ad82d339d4ae76cdcfd49473f604ed5a
ChristianGroeber <christian.groeber@bluewin.ch>
2019-04-30 12:01:18 +0200

    added links for portfolio elements

--- christian_groeber_ch/hire_me/views.py
+++ christian_groeber_ch/hire_me/views.py
@@ -12,17 +12,21 @@ def index(request):
     elements = Element.objects.all().order_by('-date_started')
     experiences = []
     for experience in exp:
-        experiences.append(ExperienceObject(experience.title, experience.date_from, description=experience.description, date_until=experience.date_until))
+        experiences.append(ExperienceObject(experience.title, experience.date_from, 'experience', description=experience.description,
+                                            date_until=experience.date_until))
     for element in elements:
-        experiences.append(ExperienceObject(element.title, element.date_started, description=element.description, date_until=element.date_finished))
-    print(experiences)
+        element.generate_url()
+        experiences.append(ExperienceObject(element.title, element.date_started, 'element', description=element.description,
+                                            date_until=element.date_finished, url=element.url))
     sorted_experiences = sorted(experiences, key=lambda test: test.date_from)
     return render(request, 'hire_me/index.html', {'resume': resume, 'experiences': sorted_experiences})
 
 
 class ExperienceObject:
-    def __init__(self, title, date_from, description=None, date_until=None):
+    def __init__(self, title, date_from, exp_type, description=None, date_until=None, url=None):
         self.title = title
         self.date_from = date_from
         self.description = description
         self.date_until = date_until
+        self.exp_type = exp_type
+        self.url = url
