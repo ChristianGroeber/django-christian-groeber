304a4a49833bbe625bf0f544f01aaee9514b3314
ChristianGroeber <christian.groeber@bluewin.ch>
2019-05-01 07:36:04 +0200

    added technology to experience overview

--- christian_groeber_ch/hire_me/views.py
+++ christian_groeber_ch/hire_me/views.py
@@ -18,14 +18,14 @@ def index(request):
     for element in elements:
         element.generate_url()
         experiences.append(ExperienceObject(element.title, element.date_started, 'element', description=element.description,
-                                            date_until=element.date_finished, url=element.portfolio_type + '/' + element.url, portfolio_type=element.portfolio_type))
+                                            date_until=element.date_finished, url=element.portfolio_type + '/' + element.url, portfolio_type=element.portfolio_type, technologies=element.technologies.all()))
         portfolio.views.get_portfolio_type_from_element(element, request)
     sorted_experiences = sorted(experiences, key=lambda test: test.date_from)
     return render(request, 'hire_me/index.html', {'resume': resume, 'experiences': sorted_experiences})
 
 
 class ExperienceObject:
-    def __init__(self, title, date_from, exp_type, description=None, date_until=None, url=None, portfolio_type=None):
+    def __init__(self, title, date_from, exp_type, description=None, date_until=None, url=None, portfolio_type=None, technologies=None):
         self.title = title
         self.date_from = date_from
         self.description = description
@@ -33,3 +33,4 @@ class ExperienceObject:
         self.exp_type = exp_type
         self.url = url
         self.portfolio_type = portfolio_type
+        self.technologies = technologies
